771c7c2d78ca310ffbbd54f890d45b33ed816c21
Anna <fatamorgana9205@gmail.com>
2014-02-09 22:15:05 +0400

    Initializing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookadvicer::Application.config.secret_key_base = 'cf6547108ed3ba4266de3a3a111277218b4ff907f7dc5027a56b0fc29c3bbe1231a715a4bf696ad96efa707ec3d1f24d501cdc64533c2da56f0b6bfec1df4b73'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bookadvicer::Application.config.session_store :cookie_store, key: '_bookadvicer_session'
